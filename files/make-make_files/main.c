0d415f5e9749dbcb87ceb45120408468a56274ee
Sébastien Bernard <sebastien.bernard@epitech.eu>
2017-12-20 13:42:51 +0100

    Added pre-made main.c and auto-open

---
+++ files/make-make_files/main.c
@@ -0,0 +1,13 @@
+/*
+** NDR PROJECT, 2017
+** Make-main
+** File description:
+** Pre-made main.c
+*/
+
+//To change this file, go to ~/bin/make-make_files/main.c
+
+int main(int ac, char **av)
+{
+	return 0;
+}
